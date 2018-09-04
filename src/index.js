faf06b8b46f15cb945ef9e0bef5711d65c161b5c
Kevin Auchoybur <k.auchoybur@highco-data.fr>
2018-09-04 14:34:48 +0200

    Interface de connexion

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
-// Code React.js
-alert("Initial chatBox");
+import React from 'react' ;
+/*Affichage*/ 
+import {render} from 'react-dom';
+/*CSS*/
+import './index.css';
+
+class Connexion extends React.Component {
+
+    render(){
+        return(
+            <div className="connexionBox">
+            <form className="connexion">
+            <input type="text" placeholder="Votre pseudo" required/>
+            <button type="submit">Accéder au chat</button>
+            </form>
+            </div>
+        )
+    }
+}
+
+    /* fonction qui affiche le rendu */
+    render(
+    <Connexion />,
+    document.getElementById('root')
+    );
+
